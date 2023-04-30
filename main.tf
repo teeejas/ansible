51a14c0bf5d81a69fe5ba9deec7891711ccac40b
root <teeejas@gmail.com>
2023-04-30 20:57:35 +0000

    create instances with new key

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = "us-east-1"
 }
-
 module "ec2-instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.0.0"
@@ -11,9 +10,9 @@ module "ec2-instance" {
   name = "instance-${each.key}"
 
   instance_type          = "t2.micro"
-  key_name               = "ansible"
+  key_name               = "ansible_tejas"
   monitoring             = false
-  vpc_security_group_ids = ["sg-041180a2067f14eec","sg-0a43ce2831de7725d"]
+  vpc_security_group_ids = ["sg-0a79bc87db0e1cbac","sg-063d6db51bb015e91"]
   ami 			 = "ami-0e54300850599e43c"
   subnet_id              = "subnet-0fb2a608ee517e54c"
 
@@ -24,7 +23,6 @@ module "ec2-instance" {
     Environment = "dev"
   }
 }  
-
 module "http_80_security-group" {
   version = "4.17.2"
   source  = "terraform-aws-modules/security-group/aws//modules/http-80"
@@ -32,7 +30,7 @@ module "http_80_security-group" {
   description = "Security group for web-server with HTTP ports open within VPC"
   vpc_id      = "vpc-00fd59baa806f6c7f"
   #ingress_cidr_blocks = ["10.0.0.0/8"]
-  ingress_cidr_blocks = ["34.229.94.7/32","10.0.102.132/32","23.30.116.205/32"]
+  ingress_cidr_blocks = ["34.229.94.7/32","73.215.214.228/32","10.0.102.132/32","23.30.116.205/32"]
 
 }
 
@@ -40,7 +38,7 @@ module "ssh_security_group" {
   source  = "terraform-aws-modules/security-group/aws//modules/ssh"
   version = "4.17.2"
 
-  ingress_cidr_blocks = ["34.229.94.7/32","10.0.102.132/32","23.30.116.205/32"]
+  ingress_cidr_blocks = ["34.229.94.7/32","10.0.102.132/32","73.215.214.228/32","23.30.116.205/32"]
   vpc_id      = "vpc-00fd59baa806f6c7f"
   description = "Security group for web-server with SSH  ports open within VPC"
   name        = "anisble SSH-server"
