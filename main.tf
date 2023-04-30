4ee6f95684b0f9661e474d1b315d8cebdbeb8f5b
root <teeejas@gmail.com>
2023-04-30 18:27:12 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2-instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.0.0"
+
+  for_each = toset(["web1", "web2", "alb-ansible"])
+
+  name = "instance-${each.key}"
+
+  instance_type          = "t2.micro"
+  key_name               = "ansible"
+  monitoring             = false
+  vpc_security_group_ids = ["sg-041180a2067f14eec","sg-0a43ce2831de7725d"]
+  ami 			 = "ami-0e54300850599e43c"
+  subnet_id              = "subnet-0fb2a608ee517e54c"
+
+
+  tags = {
+    Terraform   = "true"
+    app =	"ansible-tejas"
+    Environment = "dev"
+  }
+}  
+
+module "http_80_security-group" {
+  version = "4.17.2"
+  source  = "terraform-aws-modules/security-group/aws//modules/http-80"
+  name        = "anisble web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = "vpc-00fd59baa806f6c7f"
+  #ingress_cidr_blocks = ["10.0.0.0/8"]
+  ingress_cidr_blocks = ["34.229.94.7/32","10.0.102.132/32","23.30.116.205/32"]
+
+}
+
+module "ssh_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/ssh"
+  version = "4.17.2"
+
+  ingress_cidr_blocks = ["34.229.94.7/32","10.0.102.132/32","23.30.116.205/32"]
+  vpc_id      = "vpc-00fd59baa806f6c7f"
+  description = "Security group for web-server with SSH  ports open within VPC"
+  name        = "anisble SSH-server"
+}
+
+
